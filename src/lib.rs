441d3850e5aa87cdef9cc93e72773e6ce8ee9d53
idzeir <qiyanlong@wozine.com>
2025-07-11 10:35:28 +0800

    feat: learn diesel orm

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use std::env;
+
+use diesel::prelude::*;
+use dotenvy::dotenv;
+
+use crate::models::{NewPost, Post};
+
+pub mod models;
+pub mod schema;
+
+pub fn establish_connection() -> SqliteConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL muse be set");
+
+    SqliteConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+pub fn create_post(conn: &mut SqliteConnection, title: &str, body: &str) -> Post {
+    use crate::schema::posts;
+
+    let new_post = NewPost { title, body };
+
+    diesel::insert_into(posts::table)
+        .values(&new_post)
+        .returning(Post::as_returning())
+        .get_result(conn)
+        .expect("Error saving new post")
+}
